22b23e9a309fda0d0e075272bd19e017545a3c61
Eula Bengco <eulabengco@gmail.com>
2020-09-15 17:38:20 -0400

    coded New Question UI

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { FormControl, TextField, Button } from "@material-ui/core";
 
 export default class NewQuestion extends Component {
   render() {
     return (
       <div>
-        <p>New Question</p>
+        <p>Create New Poll:</p>
+        <FormControl>
+          Would You Rather:
+          <TextField id="filled-basic" label="Option One" variant="filled" />
+          OR
+          <TextField id="filled-basic" label="Option Two" variant="filled" />
+          <Button fullWidth variant="contained" color="primary">
+            Submit New Poll
+          </Button>
+        </FormControl>
       </div>
     );
   }
